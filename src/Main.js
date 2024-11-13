2fe424efd76514e482fa4b59744bb53de8db1b6a
Adrian Lamour <recreate3d@gmail.com>
2024-11-13 22:16:57 +0000

    Update

--- src/Main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App' // Import the App component
-
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
